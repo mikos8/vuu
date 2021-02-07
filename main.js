a46fd18933596904244a8bc5cb57f3b238ddd8e5
miraszh2020 <63465408+miraszh2020@users.noreply.github.com>
2021-02-07 15:53:48 +0600

    add notes data

--- main.js
+++ main.js
@@ -11,3 +11,23 @@ const app = Vue.createApp(App);
 // vue.createApp(App).mount('#app) 
 
 app.mount('#app'); // передаем css селектор корневого узла
+
+const App2 = {
+    data(){
+        return {
+            title: 'Note list',
+            placeholderStr: 'Enter name list',
+            inputValue:'',
+            notes:['Note1']
+        }
+    },
+    methods:{
+        inputChange(){
+            this.inputValue = event.target.value
+            console.log(event.target.value)
+        }
+    }
+}
+
+const app2 = Vue.createApp(App2);
+app2.mount('#app2')
