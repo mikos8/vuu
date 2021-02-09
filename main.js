2d21a71c139f6dcb567ec6de8e466a9d7d858501
mikos8 <65290147+mikos8@users.noreply.github.com>
2021-02-09 22:39:33 +0600

    add v-if-else touppercase

--- main.js
+++ main.js
@@ -1,44 +1,49 @@
 const App = {
-    data(){
-        return {
-            counter: 0,
-            title:'Счетчик'
-        }
-    }
+  data() {
+    return {
+      counter: 0,
+      title: "Счетчик",
+    };
+  },
 };
 
 const app = Vue.createApp(App);
-// vue.createApp(App).mount('#app) 
+// vue.createApp(App).mount('#app)
 
-app.mount('#app'); // передаем css селектор корневого узла
+app.mount("#app"); // передаем css селектор корневого узла
 
 const App2 = {
-    data(){
-        return {
-            title: 'Note list',
-            placeholderStr: 'Enter name list',
-            inputValue:'',
-            notes:['Note1']
-        }
+  data() {
+    return {
+      title: "Список заметок",
+      placeholderStr: "Введите имя заметки",
+      inputValue: "",
+      notes: [],
+    };
+  },
+  methods: {
+    inputChange(event) {
+      this.inputValue = event.target.value;
     },
-    methods:{
-        inputChange(event){
-            this.inputValue = event.target.value
-        },
-        addNewNote(){
-            this.notes.push(this.inputValue)
-            this.inputValue = ''
-        },
-        inputKeyPress(event){
-            if(event.key === 'Enter'){
-                this.addNewNote()
-            }
-        },
-        delNote(id){
-            this.notes.splice(id,1)
-        }
-    }
-}
+    addNewNote() {
+      if (this.inputValue != "") {
+        this.notes.push(this.inputValue);
+        this.inputValue = "";
+      }
+    },
+    inputKeyPress(event) {
+      if (event.key === "Enter") {
+        this.addNewNote();
+      }
+    },
+    delNote(id) {
+      this.notes.splice(id, 1);
+    },
+    toUpperCase(item) {
+      return item.toUpperCase();
+    },
+  },
+};
 
 const app2 = Vue.createApp(App2);
-app2.mount('#app2')
+app2.mount("#app2");
