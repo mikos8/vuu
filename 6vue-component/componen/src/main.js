6303375299abe7c95499d0fd8b9016894f8deeb5
miraszh2020 <63465408+miraszh2020@users.noreply.github.com>
2021-03-09 02:44:02 +0600

    123

--- 6vue-component/componen/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Header from './Header'
-import './theme.css'
-
-const app = createApp(App)
-app.mount('#app')
-
-app.component('TheHeader', Header)
